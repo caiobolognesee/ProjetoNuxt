fe5055f5fea3fd66bed9472bbbd2dfa8d975b4ba
Caio <bolognese8891@hotmail.com>
2022-06-06 18:29:23 -0300

    primeiras alteracoes - menu e paginas

---
+++ pages/empresa.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1 class="text-lg font-bold">A empresa</h1>
+      <p> teste </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/servicos.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1 class="text-lg font-bold">Serviços</h1>
+      <p>teste</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
