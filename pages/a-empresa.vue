5ee5e1928e3c505bc8fbd5f27cfb5379564e9fec
Caio <bolognese8891@hotmail.com>
2022-06-06 16:22:09 -0300

    Start Inicial

---
+++ pages/a-empresa.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="bg=gray-100">
+            <div class="conteiner mx-auto flex items-center h-10">
+                teste
+            </div>    
+        </div>
+        <h1> A empresa </h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
